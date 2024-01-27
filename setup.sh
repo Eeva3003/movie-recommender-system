f8555a0ede90d4ec57756703af6bfb27ff1bf9c1
Eeva3003 <97470217+Eeva3003@users.noreply.github.com>
2024-01-27 23:28:49 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#linking
+web:sh setup.sh && streamlit run app.py
+
+
+setting 
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
